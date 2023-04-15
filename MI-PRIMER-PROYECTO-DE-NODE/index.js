2814b212f83e243052913d17359d60de6278e07c
Diego sepulveda <125772013+darsito92@users.noreply.github.com>
2023-04-15 18:47:38 -0400

    Add files via upload

---
+++ MI-PRIMER-PROYECTO-DE-NODE/index.js
@@ -0,0 +1,12 @@
+//Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+//esta es la parte intermedia
+//en esta parte se realizan los chequeos
+//Este es el mensaje que se envia apra despedirce
+
+
+/*
+    comentarios parrafos mas grandes
+ */
+console.log("Adios");
