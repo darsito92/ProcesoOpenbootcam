874663837ff853d7421e8bb0241c88fd1aeaf61a
Diego sepulveda <125772013+darsito92@users.noreply.github.com>
2023-04-16 19:58:14 -0400

    Add files via upload

--- MI-PRIMER-PROYECTO-DE-NODE/ejercicio_2.js
+++ MI-PRIMER-PROYECTO-DE-NODE/ejercicio_2.js
@@ -6,7 +6,8 @@ const lista =[
     {
         titulo:"padre rico padre pobre",
         autor:"Robert kiyosaky",
-        fecha:new Date(1997,1,3)
+        fecha:new Date(1997,1,3),
+        url:"https://www.amazon.com/-/es/Robert-T-Kiyosaki/dp/1644731967/ref=sr_1_1?keywords=padre+rico+padre+pobre&qid=1681689432&sprefix=padre%2Caps%2C218&sr=8-1"
     },
 ];
 
