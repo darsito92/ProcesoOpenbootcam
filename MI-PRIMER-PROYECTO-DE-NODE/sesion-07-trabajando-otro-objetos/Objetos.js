198219a3a5216cfbd4acee112abad3d1aa66a891
Diego sepulveda <125772013+darsito92@users.noreply.github.com>
2023-04-25 15:43:09 -0400

    Add files via upload

--- MI-PRIMER-PROYECTO-DE-NODE/sesion-07-trabajando-otro-objetos/Objetos.js
+++ MI-PRIMER-PROYECTO-DE-NODE/sesion-07-trabajando-otro-objetos/Objetos.js
@@ -1,52 +1,25 @@
-// trabajando con objetos 
-const obj ={
-    id:4,
-    nombre:"Juan",
-    apellido:"Gonzales",
-    isDeveloper:true,
-    libros_favoritos:["el metodo","el codigo de la manifestacion"],
-    "4-juegos":[1,2,3,4]
+const persona = {
+    nombre:"diego",
+    apellido:"sepulveda",
+    edad:30,
+    altura:168,
+    eresDesarrollador:true
 }
 
-console.log(obj.id)
-console.log(obj["4-juegos"])
+const edad = persona.edad
+console.log(edad)
+const lista = [{...persona},
+    {nombre:"lesie",
+    apellido:"silva",
+    edad:29,
+    altura:155,
+    eresDesarrollador:false},
+    {nombre:"raul",
+    apellido:"santillan",
+    edad:31,
+    altura:183,
+    eresDesarrollador:false}]
 
-const prop ="isDeveloper"  //acceder a propiedades atraves de variables
-console.log(obj[prop])
 
-const obj2 = obj;
-console.log(obj2)
-
-obj2.nombre = "iñigo"
-console.log(obj2.nombre)
-console.log(obj.nombre) // cambia el obj original utiliza el espacio original no lo duplica
-
- //no pasa lo mismo con las variables
-let val1 = 4
-let val2 =val1
-val2 =6
-
-console.log(val1)
-console.log(val2) 
-
-///////////////
-const obj3 = {...obj} // forma correcta para cambiar datos solo en nuevo obj
-
-obj3.nombre = "Diego"
-console.log(obj.nombre)
-console.log(obj3.nombre)
-
-///
-
-// como ordenar listas de objetos en funcion de una propiedad
-const listaPeliculas = [
-    {titulo: "lo que el viento se llevo", anyo:1943},
-    {titulo: "Titanic", anyo:1997},
-    {titulo: "Moana", anyo:2016},
-    {titulo: "El efecto mariposa", anyo:2004},
-    {titulo: "Ted", anyo:2012}
-]
-console.log(listaPeliculas)
-// .sort() muta valor de la lista original
-listaPeliculas.sort((a,b)=>a.anyo-b.anyo)
-
+const newLista = lista.sort((a,b)=> b.edad-a.edad)
+console.log(newLista)
